662ac62e5bf223d47a59ce204375c3c2e817f3de
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-01-23 23:38:10 -0700

    Baseline next 13 project

--- src/lib/helpers.ts
+++
@@ -1,8 +0,0 @@
-export const handleSuccess = (name: string, message?: string) => {
-	console.log(message ? `✅ ${name} success: ${message}` : `✅ ${name} success!`);
-};
-
-export const handleError = (name: string, message?: string) => {
-	console.log(`❌ ${name}: ${message}`);
-	console.log(message ? `❌ ${name} error: ${message}` : `❌ ${name} error!`);
-};
--- tests/HomePage.test.ts
+++
@@ -1,5 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('homepage contents accurate', async ({ page }) => {
-	expect(true).toBeTruthy();
-});
