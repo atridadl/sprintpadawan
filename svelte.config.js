6d532d13870955073097417eadedcf282edd3d0b
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-01-25 10:38:45 -0700

    Edge?

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-vercel';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			edge: true
+		})
 	},
 	preprocess: [
 		preprocess({
-			postcss: true,
-		}),
+			postcss: true
+		})
 	],
 	vitePlugin: {
 		experimental: {
 			inspector: {
-				holdMode: true,
+				holdMode: true
 			}
 		}
-	}		
-
+	}
 };
 
 export default config;
