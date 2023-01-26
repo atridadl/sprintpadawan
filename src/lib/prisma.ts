73d2d272264f927a9c0e6c2a9342ff7155a1a8cd
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-01-25 18:50:21 -0700

    Remove global

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-	var prisma: PrismaClient; // This must be a `var` and not a `let / const`
-}
-
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+const prisma: PrismaClient = new PrismaClient();
 
 export default prisma;
