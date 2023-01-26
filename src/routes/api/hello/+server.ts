41e262a229f4dfa6246a0c3fb58b2d8cc59db470
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-01-25 22:37:50 -0700

    Misc DB experiments

--- src/routes/api/hello/+server.ts
+++ src/routes/api/hello/+server.ts
@@ -1,7 +1,8 @@
 import type { RequestHandler } from './$types';
-import prisma from '$lib/prisma';
+import PrismaClient from '$lib/prisma';
 
 export const GET = (async ({ url }) => {
+	const prisma = new PrismaClient();
 	const users = await prisma.user.findMany();
 	return new Response(String(JSON.stringify(users)));
 }) satisfies RequestHandler;
