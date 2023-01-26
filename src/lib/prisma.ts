8282961dc86fba45ea454649b7266cd55d9224a0
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-01-25 19:52:23 -0700

    Fuck

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma: PrismaClient = new PrismaClient();
+declare global {
+	var prisma: PrismaClient; // This must be a `var` and not a `let / const`
+}
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
 
 export default prisma;
