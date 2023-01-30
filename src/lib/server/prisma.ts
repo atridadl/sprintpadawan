927ce0a95c91609d50d104dc6abd9b1ce865198a
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-01-29 18:09:17 -0700

    Localhost vs Edge logic

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from '@prisma/client/edge';
-const prisma: PrismaClient = new PrismaClient();
+import { PrismaClient } from '@prisma/client';
+import { PrismaClient as PrismaClientEdge } from '@prisma/client/edge';
+import { dev } from '$app/environment';
+const prisma: PrismaClient = dev ? new PrismaClient() : new PrismaClientEdge();
 
 export default prisma;
