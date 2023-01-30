4cba13cb2e68c91b26b752622b8e1e8777e71d96
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-01-29 19:11:26 -0700

    ffs edge sucks I guess

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,7 +1,7 @@
 import Pusher from 'pusher-js';
 import { PUBLIC_PUSHER_SUB_KEY } from '$env/static/public';
 
-export const pusher = new Pusher('9a38bdc04658a111ed39', {
+export const pusher = new Pusher(PUBLIC_PUSHER_SUB_KEY, {
 	cluster: 'us3'
 });
 
