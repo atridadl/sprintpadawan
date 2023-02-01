dbcfa2e65dbcda90a4d4133c92ec76eac7ab73d2
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-02-01 10:53:34 -0700

    Update console log message for realtime messages

--- src/lib/ably.client.ts
+++ src/lib/ably.client.ts
@@ -6,7 +6,7 @@ const ably = new Ably.Realtime.Promise(PUBLIC_ABLY_SUB_KEY);
 export const subscribeToChannel = async (channel: string, event: string, callback: Function) => {
 	const eventChannel = ably.channels.get(channel);
 	await eventChannel.subscribe(event, (message) => {
-		console.log('Received a greeting message in realtime: ' + message.data);
+		console.log(`${event} received from channel ${channel}: ` + message.data);
 		callback();
 	});
 };
