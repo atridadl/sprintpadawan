0f88f2a8fd2e544ce758d51abbfcb2e5d5ec5183
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-02-01 18:20:39 -0700

    Manifest?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,8 +9,14 @@
 </script>
 
 <svelte:head>
-	<title>SprintPadawan</title>
-	<meta name="description" content="Scrum Planning Toolkit" />
+	<title>Sprint Padawan</title>
+	<meta name="description" content="Plan. Sprint. Repeat." />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="manifest" href="/site.webmanifest" />
+	<meta name="msapplication-TileColor" content="#da532c" />
+	<meta name="theme-color" content="#ffffff" />
 </svelte:head>
 
 <AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
