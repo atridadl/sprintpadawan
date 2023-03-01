86a5a002000d85358915b08c1afc8be9b84456f3
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-03-01 13:32:52 -0700

    Nevermind.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,13 @@
 import type { PageServerLoad } from './$types';
 import type { ExtendedSession } from '$lib/types';
-import type PrismaEdge from '@prisma/client/edge';
+import type { Room } from '@prisma/client';
 import { VERCEL_ENV } from '$env/static/private';
 import type { Config } from '@sveltejs/kit';
-
-export const config: Config = {
-	runtime: 'edge'
-};
 
 export const load: PageServerLoad = (async ({ fetch, locals }) => {
 	const session: ExtendedSession = (await locals.getSession()) as ExtendedSession;
 	const roomsResponse: Response = await fetch('/api/room');
-	const rooms: PrismaEdge.Room[] = await roomsResponse.json();
+	const rooms: Room[] = await roomsResponse.json();
 
 	return {
 		session,
